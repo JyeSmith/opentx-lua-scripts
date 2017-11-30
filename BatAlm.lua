1d15564eab0a855cbdcf07850dc2f109d2e27dc9
Jye Smith <jyesmith@gmail.com>
2017-11-30 21:39:19 +1000

    Update BatAlm.lua

--- BatAlm.lua
+++ BatAlm.lua
@@ -5,13 +5,20 @@
 -- CONFIG --
 
 local minVoltage = 14.4 -- minimum voltage before announcment
-local delayInSeconds = 4.0 -- delay in seconds between announcments
+local delayTimeInSeconds = 6.0 -- delay in seconds between announcments
+local sagTimeInSeconds = 4.0 -- delay in seconds between announcments
 
 -- DON'T EDIT BELOW THIS LINE --
 
 local unitVolts = 1 -- Sets voltage units to be announced
 local battVolts = 0
 local timeOfLastAnnouncment = 0
+local timeBelowMinVoltage = 0
+local rssi, alarm_low, alarm_crit = getRSSI()
+
+function round(num, numDecimalPlaces)
+  return tonumber(string.format("%." .. (numDecimalPlaces or 0) .. "f", num))
+end
 
 local function init_func()
   -- init_func is called once when model is loaded
@@ -20,13 +27,22 @@ end
 local function bg_func()
   -- bg_func is called periodically (always, the screen visibility does not matter)
   
-  if getRSSI() then
+  rssi, alarm_low, alarm_crit = getRSSI()
+  
+  if rssi > 0 then
     
-    battVolts = getValue(vfas)
+    battVolts = round(getValue("VFAS"), 1)
     
-    if getTime() > timeOfLastAnnouncment + (delayInSeconds * 100) and battVolts < minVoltage then
-      playNumber(battVolts, unitVolts)
-      timeOfLastAnnouncment = gettime()
+    if battVolts <= minVoltage and getTime() > (timeOfLastAnnouncment + (delayTimeInSeconds * 100)) then
+      if timeBelowMinVoltage == 0 then 
+        timeBelowMinVoltage = getTime()
+      end
+      if getTime() > (timeBelowMinVoltage + (sagTimeInSeconds * 100)) then
+        playNumber(battVolts*10, unitVolts, PREC1)
+        timeOfLastAnnouncment = getTime()
+      end
+    elseif battVolts > minVoltage then
+      timeBelowMinVoltage = 0
     end -- delay
   
   end -- getRSSI()
@@ -38,12 +54,14 @@ local function run_func(event)
   
   lcd.clear()  
 	
-  if getRSSI() then
-    if battVolts < minVoltage then
-      lcd.drawText(30, 20, battVolts .. "V", XXLSIZE + INVERS + BLINK )
-    else
-      lcd.drawText(30, 20, battVolts .. "V", XXLSIZE )  
+  if rssi > 0 then
+    if battVolts <= minVoltage then
+      lcd.drawText(25, 15, battVolts, XXLSIZE + BLINK )
+    else 
+      lcd.drawText(25, 15, battVolts, XXLSIZE )
     end
+  else
+    lcd.drawText(25, 15, "00.0", XXLSIZE )
   end
 
 end
