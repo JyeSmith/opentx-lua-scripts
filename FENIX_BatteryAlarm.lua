12a3f979bc63be83ff310d0aa7b095f2858201a7
Jye Smith <jyesmith@gmail.com>
2017-11-30 13:42:37 +1000

    added comments

--- FENIX_BatteryAlarm.lua
+++ FENIX_BatteryAlarm.lua
@@ -4,14 +4,14 @@
 
 -- CONFIG --
 
-minVoltage = 14.4 -- minimum voltage before announcment
-delayInSeconds = 4.0 -- delay in seconds between announcments
+local minVoltage = 14.4 -- minimum voltage before announcment
+local delayInSeconds = 4.0 -- delay in seconds between announcments
 
 -- DON'T EDIT BELOW THIS LINE --
 
-unitVolts = 1
-battVolts = 0
-timeOfLastAnnouncment = 0
+local unitVolts = 1 -- Sets voltage units to be announced
+local battVolts = 0
+local timeOfLastAnnouncment = 0
 
 local function init_func()
   -- init_func is called once when model is loaded
@@ -24,26 +24,28 @@ local function bg_func()
     
     battVolts = getValue(vfas)
     
-	if getTime() < timeOfLastAnnouncment + (delayInSeconds * 100) and battVolts < minVoltage then
+    if getTime() > timeOfLastAnnouncment + (delayInSeconds * 100) and battVolts < minVoltage then
       playNumber(battVolts, unitVolts)
       timeOfLastAnnouncment = gettime()
-	end -- delay
-	
-  end -- getRSSI()
+    end -- delay
   
+  end -- getRSSI()
+
 end 
 
 local function run_func(event)
   -- run_func is called periodically only when screen is visible
   
   lcd.clear()  
-  
-  if battVolts < minVoltage then
-    lcd.drawText(30, 20, battVolts .. "V", XXLSIZE + INVERS + BLINK )
-  else
-    lcd.drawText(30, 20, battVolts .. "V", XXLSIZE )  
+	
+  if getRSSI() then
+    if battVolts < minVoltage then
+      lcd.drawText(30, 20, battVolts .. "V", XXLSIZE + INVERS + BLINK )
+    else
+      lcd.drawText(30, 20, battVolts .. "V", XXLSIZE )  
+    end
   end
-    
+
 end
 
 return { run=run_func, background=bg_func, init=init_func  }
