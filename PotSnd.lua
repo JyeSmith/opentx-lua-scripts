7147ad7f1746da8bc0e6638811671d5d58b675ad
Jye Smith <jyesmith@gmail.com>
2017-12-04 10:13:59 +1000

    Create PotSnd.lua
    A lua script for playing sounds when varying a potentiometer e.g. the pot is setup to do something within Betaflight and you need audio feedback from the Tx.

---
+++ PotSnd.lua
@@ -0,0 +1,27 @@
+-- PotSnd.lua
+-- https://github.com/jyesmith/opentx-lua-scripts
+-- By Jye Smith
+
+WARNING - UNTESTED! THIS MESSAGE WILL BE REMOVED ONCE TESTED.
+
+local function run(event)
+
+  local filePlayed = false
+  local potValue = getValue("s1")            -- http://downloads-20.open-tx.org/firmware/lua_fields.txt
+
+  if potValue < 1200 and ~filePlayed then    -- https://opentx.gitbooks.io/opentx-2-2-lua-reference-guide/general/playFile.html
+    playFile(low_rates)
+    filePlayed = true
+  elseif potValue > 1400 and potValue < 1600 and ~filePlayed then
+    playFile(mid_rates)
+    filePlayed = true
+  elseif potValue > 1800 and ~filePlayed then
+    playFile(high_rates)
+    filePlayed = true
+  else
+    filePlayed = false
+  end
+
+end
+
+return { run=run }
