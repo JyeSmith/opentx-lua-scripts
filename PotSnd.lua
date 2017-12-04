9113640e0dad6cb3464f3d606f70bd3dd6314aa6
Jye Smith <jyesmith@gmail.com>
2017-12-04 10:16:11 +1000

    Update PotSnd.lua

--- PotSnd.lua
+++ PotSnd.lua
@@ -4,13 +4,14 @@
 
 -- WARNING - UNTESTED! THIS MESSAGE WILL BE REMOVED ONCE TESTED.
 
+local filePlayed = false
+
 local function run(event)
 
-  local filePlayed = false
-  local potValue = getValue("s1")            -- http://downloads-20.open-tx.org/firmware/lua_fields.txt
+  local potValue = getValue("s1")             -- http://downloads-20.open-tx.org/firmware/lua_fields.txt
 
-  if potValue < 1200 and ~filePlayed then    -- https://opentx.gitbooks.io/opentx-2-2-lua-reference-guide/general/playFile.html
-    playFile(low_rates)
+  if potValue < 1200 and ~filePlayed then
+    playFile(low_rates)                       -- https://opentx.gitbooks.io/opentx-2-2-lua-reference-guide/general/playFile.html
     filePlayed = true
   elseif potValue > 1400 and potValue < 1600 and ~filePlayed then
     playFile(mid_rates)
